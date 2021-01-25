d2cd9d91ef288998a06c4e9a2a95624e63bf3915
Andres Villalba Gaviria <66088651+andresvg8@users.noreply.github.com>
2021-01-24 22:15:30 -0500

    Method update created in CustomerController.java

--- src/main/java/com/andresvg/tiendavirtualdemo/controller/CustomerController.java
+++ src/main/java/com/andresvg/tiendavirtualdemo/controller/CustomerController.java
@@ -2,12 +2,14 @@ package com.andresvg.tiendavirtualdemo.controller;
 
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +41,20 @@ public class CustomerController {
 		else {
 			return ResponseEntity.notFound().build();
 		}
+	}
+	
+	// Update a customer
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@RequestBody Customer customerDetails, @PathVariable(value="id") Long userId){
+		Optional<Customer> oCustomer=cs.findById(userId);
+		if( !oCustomer.isPresent() ) {
+			return ResponseEntity.notFound().build();
+		}
+		/*oCustomer.get().setEmail(customerDetails.getEmail());
+		oCustomer.get().setEnabled(customerDetails.getEnabled());
+		oCustomer.get().setFirstName(customerDetails.getFirstName());
+		oCustomer.get().setLastName(customerDetails.getLastName());*/
+		BeanUtils.copyProperties(customerDetails, oCustomer);
+		return ResponseEntity.status(HttpStatus.CREATED).body(cs.save(oCustomer.get()));
 	}
 }
